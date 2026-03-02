6aa8840acb6a64cdeb3c49d7ed6a34a958b8727c
kyawzinIT99 <kyawzin.ccna@gmail.com>
2026-03-02 14:31:31 +0700

    Rename Modal app to CulinaryAI

--- modal_server.py
+++ modal_server.py
@@ -3,20 +3,23 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import os
 
-app = modal.App("kyawzin-ccna")
+app = modal.App("CulinaryAI")
 
-image = modal.Image.debian_slim().pip_install("fastapi", "starlette")
+image = (
+    modal.Image.debian_slim()
+    .pip_install("fastapi", "starlette")
+    .add_local_dir(".", remote_path="/assets")
+)
 
 web_app = FastAPI()
 
 # Mount the static files
 # The current directory will be mounted to /assets in the container
-web_app.mount("/", StaticFiles(directory="/assets", html=True), name="static")
+web_app.mount("/", StaticFiles(directory="/assets", html=True, check_dir=False), name="static")
 
 
 @app.function(
     image=image,
-    mounts=[modal.Mount.from_local_dir(".", remote_path="/assets")],
     allow_concurrent_inputs=100
 )
 @modal.asgi_app()
